38a83025f072d26ca197e4a9044328c876d6ad5f
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-03-20 17:05:32 +0200

    add to-do list template with note management functionality

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -7,3 +9,31 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/to-do-list', methods=['GET', 'POST'])
+@login_required
+def to_do_list():
+    if request.method == 'POST':
+        note_data = request.form.get('note')
+        if len(note_data.strip()) < 1:
+            flash('Note cannot be empty!', category='error')
+        else:
+            new_note = Note(data=note_data, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+        return redirect(url_for('views.to_do_list'))
+    return render_template("to_do_list.html", user=current_user)
+
+@views.route('/delete-note/<int:note_id>', methods=['POST'])
+@login_required
+def delete_note(note_id):
+    note = Note.query.get(note_id)
+    if note and note.user_id == current_user.id:  
+        db.session.delete(note)
+        db.session.commit()
+        flash('Note deleted!', category='success')
+    else:
+        flash('Note not found or unauthorized action.', category='error')
+    return redirect(url_for('views.to_do_list'))
+
