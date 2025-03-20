faa454c609a786047301020ec78bfc47935f2b5c
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-03-20 15:49:44 +0200

    adding welcome page

--- website/views.py
+++ website/views.py
@@ -1,9 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route("/")
+def welcome():
+    if current_user.is_authenticated:
+        return redirect(url_for("views.home"))
+    return render_template("welcome.html", user=current_user)
+
+
+@views.route('/home')
 @login_required
 def home():
     return render_template("home.html", user=current_user)
