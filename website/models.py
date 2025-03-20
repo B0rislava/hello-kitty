5cd9206e6a6ff7b7798c802688ef5a13714941fb
loraiv <102684937+loraiv@users.noreply.github.com>
2025-03-20 20:40:42 +0200

    add db models

--- website/models.py
+++ website/models.py
@@ -6,3 +6,27 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150),nullable=False)
     first_name = db.Column(db.String(150))
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
+    answers = db.relationship('Answer', backref='question', lazy=True)
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+    text = db.Column(db.String(500), nullable=False)
+    is_correct = db.Column(db.Boolean, nullable=False, default=False)
+
+class UserAnswer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id'), nullable=False)
+
+
+    @staticmethod
+    def create_answer(question_id, answer_text, is_correct=False):
+        new_answer = Answer(question_id=question_id, text=answer_text, is_correct=is_correct)
+        db.session.add(new_answer)
+        db.session.commit()
+        return new_answer
