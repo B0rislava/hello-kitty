83576f333e99b3ea83a9063702f43ee5177ec51b
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-03-20 18:17:29 +0200

    adding 'profie' to navigation

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
@@ -14,3 +14,14 @@ def welcome():
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/profile')
+@login_required
+def profile():
+    return render_template("profile.html", user=current_user)
+
+
+@views.route('/redirect_to_profile')
+def redirect_to_profile():
+    flash("Sign in first!", "error")  # Запазва съобщението в session
+    return redirect(url_for("auth.login"))  # Пренасочване към login
